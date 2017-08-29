a57f30de7a643dfdca4b8929b019c41d34124477
Pierre-Antoine MARTIN <pierre.antoine.martin.pro@gmail.com>
2017-08-29 11:34:25 +0200

    ajout menu

---
+++ src/menu/menu.component.js
@@ -0,0 +1,9 @@
+import templateMenu from "./menuVue.html"
+import MenuController from "./menu.controller"
+
+const menuComponent = {
+    template: templateMenu,
+    controller: MenuController
+}
+
+export default menuComponent
---
+++ src/menu/menu.controller.js
@@ -0,0 +1,8 @@
+
+export default class MenuController {
+    
+        constructor() {
+    
+        }
+    
+}
