4118f34e689ad4014bda0beef4144de64695207e
Anthony Frugier <fru.anthony@gmail.com>
2017-08-29 14:11:24 +0200

    enregistrement en localStorage

--- src/utils/apiUrl.service.js
+++
@@ -1,7 +0,0 @@
-const apiUrl = {
-    pizzas : 'http://localhost:3000/pizzas',
-    commandes : 'http://localhost:3000/commandes',
-    user : 'http://localhost:3000/utilisateurs'
-}
-
-export default apiUrl
