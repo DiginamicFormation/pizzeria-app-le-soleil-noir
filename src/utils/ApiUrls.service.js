d36d1957e8a49c0ad6c046a51e8f1650972a7619
Thomas Kuntz <Thomas.kuntz68@gmail.com>
2017-08-29 17:29:41 +0200

    suppression  src/utils/ApiUrls.service.js

--- src/utils/ApiUrls.service.js
+++
@@ -1,6 +0,0 @@
-const apiUrls = {
-  pizzas: "http://localhost:3000/pizzas",
-  commandes: "http://localhost:3000/commandes",
-  utilisateurs: "http://localhost:3000/utilisateurs"
-};
-export default apiUrls;
